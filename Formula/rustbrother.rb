97bf1823390f8f9d4b20c409797b5d9951501ed4
MASK <m.a.sobergklyver@gmail.com>
2025-06-05 22:31:56 +0200

    Update RustBrother formula to version 0.1.15 and add SHA256 checksums for all platforms

--- Formula/rustbrother.rb
+++ Formula/rustbrother.rb
@@ -1,20 +1,20 @@
 class Rustbrother < Formula
   desc "Hunt down unused CSS in React components and detect complex patterns"
   homepage "https://github.com/MarkusAugust/RustBrother"
-  version "0.1.0"
+  version "0.1.15"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/MarkusAugust/RustBrother/releases/download/v#{version}/rustbrother-darwin-arm64.tar.gz"
-      sha256 "PUT_DARWIN_ARM64_SHA256_HERE"
+      sha256 "ea50a5fa9626a505a34f5595004673863e3c2199ce341de7adbed58fb8103ebb"
     else
       url "https://github.com/MarkusAugust/RustBrother/releases/download/v#{version}/rustbrother-darwin-amd64.tar.gz"
-      sha256 "PUT_DARWIN_AMD64_SHA256_HERE"
+      sha256 "4d0786041f9e5c4231ac7ea22309981914ab38409716b298afadf53f78901078"
     end
   elsif OS.linux?
     url "https://github.com/MarkusAugust/RustBrother/releases/download/v#{version}/rustbrother-linux-amd64.tar.gz"
-    sha256 "PUT_LINUX_AMD64_SHA256_HERE"
+    sha256 "2b8411b158853a787bf206d085aaacf682e0f0557b206496111d5359ba9d9723"
   end
 
   def install
